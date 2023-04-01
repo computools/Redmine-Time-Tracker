5124406f8d34a43a7949cee64da5b7682c3860b4
Alexander Zakharov <sys@eml.ru>
2023-04-01 11:50:57 +0300

    update main windows

--- include/mainwindow.h
+++ include/mainwindow.h
@@ -1,43 +1,52 @@
-#ifndef MAINWINDOW_H
-#define MAINWINDOW_H
+#ifndef MAIN_WINDOW_H
+#define MAIN_WINDOW_H
 
 #include <QMainWindow>
 #include "view/clockview.h"
 
 class QSystemTrayIcon;
+
 class ClockView;
+
 class AdvancedSettings;
 
 namespace Ui {
-class MainWindow;
+    class MainWindow;
 }
 
 class MainWindow : public QMainWindow {
-  Q_OBJECT
+Q_OBJECT
 public:
-  explicit MainWindow(QWidget *parent = 0);
-  ~MainWindow();
+    explicit MainWindow(QWidget *parent = nullptr);
 
-  void initDefaultMenu();
-  void initActions();
+    ~MainWindow() override;
+
+    void initDefaultMenu();
+
+    void initActions();
 
 protected:
-  void closeEvent(QCloseEvent *event);
-  void timerEvent(QTimerEvent *);
+    void closeEvent(QCloseEvent *event) override;
+
+    void timerEvent(QTimerEvent *) override;
 
 private slots:
-  void actionStart();
-  void actionPause();
-  void actionStop();
-  void setState(ClockState state);
+
+    void actionStart();
+
+    void actionPause();
+
+    void actionStop();
+
+    void setState(ClockState clockState);
 
 private:
-  Ui::MainWindow *ui;
-  QSystemTrayIcon *systemTrayIcon;
-  quint64 seconds;
-  ClockView *clock;
-  AdvancedSettings *settings;
-  ClockState state;
+    Ui::MainWindow *ui;
+    QSystemTrayIcon *systemTrayIcon;
+    quint64 seconds;
+    ClockView *clock;
+    AdvancedSettings *settings;
+    ClockState state;
 };
 
-#endif // MAINWINDOW_H
+#endif // MAIN_WINDOW_H
